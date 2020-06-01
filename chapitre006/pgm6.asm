9899cd4a5176289c3855a098cb0b221fdae64136
vincentARM <51370770+vincentARM@users.noreply.github.com>
2020-06-01 18:29:15 +0200

    Update pgm6.asm

--- chapitre006/pgm6.asm
+++ chapitre006/pgm6.asm
@@ -218,7 +218,7 @@ afficherReg:
     call afficherMess
     ;pop eax              ; restaur des registres
     leave                ; epilogue
-    ret
+    ret 4
 ;************************************************************
 ;           conversion registre en chaine décimale
 ;************************************************************
