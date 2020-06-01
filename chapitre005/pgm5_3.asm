98165d7571c9964d5ff4790dc6ff7d5b063aaf73
vincentARM <51370770+vincentARM@users.noreply.github.com>
2020-06-01 18:27:15 +0200

    Update pgm5_3.asm

--- chapitre005/pgm5_3.asm
+++ chapitre005/pgm5_3.asm
@@ -119,7 +119,7 @@ afficherReg:
     call afficherMess
     ;pop eax              ; restaur des registres
     leave                ; epilogue
-    ret
+    ret 4
 ;************************************************************
 ;           conversion registre en chaine décimale
 ;************************************************************
